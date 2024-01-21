a676afb261a85f3fca1da0a00209ef21cff45711
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-21 14:02:52 -0600

    test: update tests with utilities

--- tests/create-json-file.test.js
+++ tests/create-json-file.test.js
@@ -2,20 +2,7 @@
 
 const test = require('ava')
 const PandaDB = require('../lib/pandadb')
-const { statSync, unlinkSync } = require('node:fs')
-
-function fileExistsSync (path) {
-  try {
-    const stats = statSync(path)
-    return stats.isFile()
-  } catch (_err) {
-    return false
-  }
-}
-
-function deleteFileSync (path) {
-  unlinkSync(path)
-}
+const { fileExistsSync, deleteFileSync } = require('./utils')
 
 const defaultFile = 'pandadb.json'
 const customFile = 'this-is-my-custom-file.json'
