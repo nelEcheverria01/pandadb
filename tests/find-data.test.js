359ab1a73c08b6e250ff4740f74f961faa8ff835
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-21 17:10:48 -0600

    test: add test for find method

---
+++ tests/find-data.test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const test = require('ava')
+const PandaDB = require('../lib/pandadb')
+const { deleteFileSync } = require('./utils')
+
+const db = new PandaDB('NBA.json')
+const players = db.collection('players')
+
+test.before(async _t => {
+  await players.createMany([
+    { name: 'Lebron James', team: 'Leakers', shirtNumber: 23 },
+    { name: 'Kyle Irving', team: 'Celtics', shirtNumber: 11 }
+  ])
+})
+
+test.after(_t => {
+  deleteFileSync(db.path)
+})
+
+test('find(), should return a promise with the element found based on the query', async t => {
+  const result = await players.find({ name: 'Kyle Irving' })
+  const expected = { name: 'Kyle Irving', team: 'Celtics', shirtNumber: 11 }
+
+  t.truthy(result.id)
+  t.assert(typeof result.id === 'string')
+
+  t.is(result.name, expected.name)
+  t.is(result.team, expected.team)
+  t.is(result.shirtNumber, expected.shirtNumber)
+})
