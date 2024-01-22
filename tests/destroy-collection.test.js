b1332c9413c9d1ed3e9a77ddd46b1da2c9f3ff99
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-21 20:03:46 -0600

    add test for destroy() method

---
+++ tests/destroy-collection.test.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const test = require('ava')
+const PandaDB = require('../lib/pandadb')
+const { deleteFileSync } = require('./utils')
+
+const db = new PandaDB('NBA.json')
+const players = db.collection('players')
+
+test.before(async _t => {
+  await players.createMany([
+    { name: 'Michael Jordan', team: 'Chicago Bulls', shirtNumber: 23 },
+    { name: 'Kyle Irving', team: 'Celtics', shirtNumber: 11 },
+    { name: 'Kobe Bryant', team: 'Leakers', shirtNumber: 24 }
+  ])
+})
+
+test.after(_t => {
+  deleteFileSync(db.path)
+})
+
+test('when the destroy method is called, it will delete the collection from which it is called', async t => {
+  const response = await players.destroy()
+
+  t.is(response.status, 'success')
+  t.is(response.destroyedCollection, 'players')
+  t.false(db.collections.includes('players'))
+
+  /**
+   * The collection no longer exists but the
+   * object on which the collection was established
+   * will still be accessible, but all its operations
+   * will be ignored, (they will return undefined)
+   */
+  t.is(await players.list(), undefined)
+})
