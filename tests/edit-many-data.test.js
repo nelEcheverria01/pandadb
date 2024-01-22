a7819d6df5df0f19f2f63a029b24e319e8c6764e
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-21 19:14:36 -0600

    add test to the editMany method

---
+++ tests/edit-many-data.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const test = require('ava')
+const PandaDB = require('../lib/pandadb')
+const { deleteFileSync } = require('./utils')
+
+const db = new PandaDB('NBA.json')
+const players = db.collection('players')
+
+test.before(async _t => {
+  await players.createMany([
+    { name: 'Michael Jordan', team: 'Chicago Bulls', shirtNumber: 23 },
+    { name: 'Lebron James', team: 'Leakers', shirtNumber: 23 },
+    { name: 'Kyle Irving', team: 'Celtics', shirtNumber: 11 },
+    { name: 'Kobe Bryant', team: 'Leakers', shirtNumber: 24 }
+  ])
+})
+
+test.after(_t => {
+  deleteFileSync(db.path)
+})
+
+test(`editMany, should update the objects matching the query and return
+      a response object with information about the action performed`, async t => {
+  const response = await players.editMany({ team: 'Leakers' }, { championships: 60 })
+
+  t.is(response.status, 'success')
+  t.is(response.collection, 'players')
+  t.is(response.updatedItems, 2)
+})
