65855555539ac3355f92affa39694e05b2909eba
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-22 16:06:16 -0600

    test: update and add test to check type validations in the createMany method

--- tests/create-many-data.test.js
+++ tests/create-many-data.test.js
@@ -5,6 +5,7 @@ const PandaDB = require('../lib/pandadb')
 const { deleteFileSync } = require('./utils')
 
 const db = new PandaDB('NBA.json')
+const players = db.collection('players')
 
 test.after(_t => {
   deleteFileSync(db.path)
@@ -12,7 +13,6 @@ test.after(_t => {
 
 test(`to create a lot of data, you have to call createMany(), which will
       save the data array passed by parameter to the database (JSON file)`, async t => {
-  const players = db.collection('players')
   const response = await players.createMany([
     { name: 'Michael Jordan', team: 'Chicago Bulls', shirtNumber: 23 },
     { name: 'Lebron James', team: 'Leakers', shirtNumber: 23 },
@@ -22,4 +22,12 @@ test(`to create a lot of data, you have to call createMany(), which will
   t.is(response.status, 'success')
   t.is(response.collection, 'players')
   t.is(response.createdItems, 3)
+})
+
+test('the createMany method should crash when the input is not an array of objects', async t => {
+  await t.throwsAsync(async () => await players.createMany({}))
+  await t.throwsAsync(async () => await players.createMany(78545))
+  await t.throwsAsync(async () => await players.createMany(true))
+  await t.throwsAsync(async () => await players.createMany([1, 2, 3]))
+  await t.throwsAsync(async () => await players.createMany([[], true, false, 2]))
 })
