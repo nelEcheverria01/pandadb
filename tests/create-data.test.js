09ca47f6277c095aaa567ce181233083e1235dd2
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-22 15:59:24 -0600

    add test for type validation in the input of the create() method

--- tests/create-data.test.js
+++ tests/create-data.test.js
@@ -5,6 +5,7 @@ const PandaDB = require('../lib/pandadb')
 const { deleteFileSync } = require('./utils')
 
 const db = new PandaDB('NBA.json')
+const players = db.collection('players')
 
 test.after(_t => {
   deleteFileSync(db.path)
@@ -13,10 +14,16 @@ test.after(_t => {
 test(`create(), should create the data passed by parameter in the JSON file in
       question and return a response object with information about the operation`,
 async t => {
-  const players = db.collection('players')
   const response = await players.create({ name: 'Michael Jordan', team: 'Chicago Bulls', shirtNumber: 23 })
 
   t.is(response.status, 'success')
   t.is(response.collection, 'players')
   t.is(response.createdItems, 1)
+})
+
+test('when the data to be created is not of type object, the create() method returns an error', async t => {
+  await t.throwsAsync(async () => await players.create('data'))
+  await t.throwsAsync(async () => await players.create(45))
+  await t.throwsAsync(async () => await players.create(false))
+  await t.throwsAsync(async () => await players.create([]))
 })
