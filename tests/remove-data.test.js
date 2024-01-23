7dcb746f72ca6fa8cfd683707cf0e391556ed8df
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-22 21:05:50 -0600

    test: add a new test for the remove method to check that an error will be thrown when no data to remove is found

--- tests/remove-data.test.js
+++ tests/remove-data.test.js
@@ -15,13 +15,18 @@ test.after(_t => {
   deleteFileSync(db.path)
 })
 
-test(`to remove data, you must call remove() and it will remove the first
-      element that matches the query, and return a response object, with
-      information about the operation performed`,
-async t => {
+test(`to remove data, you must call remove() and it will remove the first element that
+      matches the query, and return a response object, with information about the operation performed`, async t => {
   const response = await players.remove({ name: 'Kobe Bryant' })
 
   t.is(response.status, 'success')
   t.is(response.collection, 'players')
   t.is(response.removedItems, 1)
+})
+
+test('when the remove method does not find data matching the query, it will throw an error', async t => {
+  const err = await t.throwsAsync(async () => await players.remove({ blablabla: 'blalblabla...' }))
+
+  t.is(err.message, 'no element found in collection players that matches the query')
+  t.assert(err instanceof Error)
 })
