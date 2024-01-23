4b02763e304bb44db44135f5539d6fedb8ccbd74
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-22 19:14:15 -0600

    add message property to assertion

--- tests/create-an-existing-collection.test.js
+++ tests/create-an-existing-collection.test.js
@@ -11,6 +11,9 @@ test.after(_t => {
 })
 
 test('when an existing collection is created, the collection() method will throw an error', t => {
+  const collection = 'sample'
+  const expectedMsg = `the ${collection} collection already exists`
+
   db.collection('sample')
-  t.throws(() => db.collection('sample'))
+  t.throws(() => db.collection('sample'), { message: expectedMsg })
 })
