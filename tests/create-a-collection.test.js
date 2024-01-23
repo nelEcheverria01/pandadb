b7c0bd7615465efef94c66aed515d22258e47f62
Nelson Echeverria <149851053+nelEcheverria01@users.noreply.github.com>
2024-01-22 18:29:48 -0600

    refactor test components

--- tests/create-a-collection.test.js
+++ tests/create-a-collection.test.js
@@ -15,11 +15,13 @@ test('collection(), will insert the collection into the database (JSON file) and
   t.assert(typeof students === 'object')
 })
 
-test.todo('the returned object will contain methods to manipulate the collection data')
+test.todo('the object returned by the collection() method will contain methods to manipulate the collection data')
 
 test('when the collection name is not of type string, collection() should return an error', t => {
-  t.throws(() => db.collection(7897))
-  t.throws(() => db.collection({}))
-  t.throws(() => db.collection(false))
-  t.throws(() => db.collection([]))
+  const expectedMsg = 'the collection name must be of type string'
+
+  t.throws(() => db.collection(100), { message: expectedMsg })
+  t.throws(() => db.collection(true), { message: expectedMsg })
+  t.throws(() => db.collection({}), { message: expectedMsg })
+  t.throws(() => db.collection([]), { message: expectedMsg })
 })
